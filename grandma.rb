f56ee73faa541a4c7d461cb6433e9f026aab29dd
Nick Bazaldua <bazalnick@gmail.com>
2020-07-27 21:51:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,4 +2,4 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    return ""
+    return "NO! NOT SINCE 1938!"
